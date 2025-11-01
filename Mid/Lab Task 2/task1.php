70fb20e461410bc65c3f57cb1deb7e66e3f85468
Sadia51934 <sadiatushi456@gmail.com>
2025-11-02 02:30:29 +0600

    Added Lab Tak 2 code

---
+++ Mid/Lab Task 2/task1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Document</title>
+    </head>
+    <body>
+        <center>
+            <h2>Welcome to new website</h2>
+        </center>
+        <p>
+            Hello Everyone. My name is Sadia Islam. I'm studying Bsc in CSE. This is my first time doing html.
+        </p>
+    </body>
+</html>
